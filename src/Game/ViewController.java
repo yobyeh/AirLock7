1d02830e972c4a3a5c20a89f657cbd104e11cd42
John Patrick <yobyeh@gmail.com>
2023-04-09 01:31:59 -0400

    basic UI

--- src/Game/ViewController.java
+++ src/Game/ViewController.java
@@ -11,104 +11,222 @@ public class ViewController {
 public void startDisplay(){
 
     JFrame frame = new JFrame("AirLock7");
-    frame.setSize(800, 400);
+    frame.setSize(1200, 600);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     JPanel panelMain = new JPanel();
     panelMain.setLayout(new GridBagLayout());
+    panelMain.setSize(1200, 600);
     GridBagConstraints constraints = new GridBagConstraints();
-
-    // JPanel panel1 = new JPanel();
-    // panel1.setLayout(new GridBagLayout());
-
-    // JPanel panel2 = new JPanel();
-    // panel2.setLayout(new GridBagLayout());
-
-    // JPanel panel3 = new JPanel();
-    // panel3.setLayout(new GridBagLayout());
-
+    
     //Create labels with fixed size
-    JLabel label1 = new JLabel("Label 1");
+    JLabel label1 = new JLabel("");
+    label1.setBackground(Color.gray);
+    label1.setOpaque(true);
     constraints.gridx = 0;
     constraints.gridy = 0;
-    constraints.gridwidth = 1;
-    constraints.gridheight = 2;
-    // constraints.weightx = 1;
-    // constraints.weighty = 1;
+    constraints.gridwidth = 2;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
     constraints.fill = GridBagConstraints.BOTH;
     panelMain.add(label1, constraints);
 
-    JTextArea textArea = new JTextArea("Text Area");
-      constraints.gridx = 1;
-      constraints.gridy = 0;
-      constraints.gridwidth = 1;
-      constraints.gridheight = 2;
-      //constraints.weightx = 1;
-      //constraints.weighty = 1;
-      constraints.fill = GridBagConstraints.BOTH;
-    panelMain.add(textArea, constraints);
+    JLabel text = new JLabel("Text", SwingConstants.CENTER);
+    //text.setBackground(Color.red);
+    //text.setOpaque(true);
+    constraints = new GridBagConstraints();
+    constraints.gridx = 2;
+    constraints.gridy = 0;
+    constraints.gridwidth = 4;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(text, constraints);
 
-    //   JLabel label3 = new JLabel("Label 3");
-    //   constraints.gridx = 2;
-    //   constraints.gridy = 0;
-    //   constraints.gridwidth = 1;
-    //   constraints.gridheight = 2;
-    //   constraints.weightx = 1;
-    //   constraints.weighty = 1;
-    //   constraints.fill = GridBagConstraints.BOTH;
-    //   panel1.add(label3, constraints);
+    JLabel label2 = new JLabel("");
+    label2.setBackground(Color.gray);
+    label2.setOpaque(true);
+    constraints = new GridBagConstraints();
+    constraints.gridx = 6;
+    constraints.gridy = 0;
+    constraints.gridwidth = 2;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(label2, constraints);
 
-    //   JLabel label4 = new JLabel("Label 4");
-    //   constraints.gridx = 0;
-    //   constraints.gridy = 0;
-    //   constraints.gridwidth = 1;
-    //   constraints.gridheight = 1;
-    //   constraints.weightx = 1;
-    //   constraints.weighty = 1;
-    //   constraints.fill = GridBagConstraints.BOTH;
-    //   panel2.add(label4, constraints);
+    JLabel questBox = new JLabel("Quest", SwingConstants.CENTER);
+    constraints = new GridBagConstraints();
+    constraints.gridx = 0;
+    constraints.gridy = 1;
+    constraints.gridwidth = 5;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(questBox, constraints);
 
-    //   JLabel label6 = new JLabel("Label 6");
-    //   constraints.gridx = 1;
-    //   constraints.gridy = 0;
-    //   constraints.gridwidth = 1;
-    //   constraints.gridheight = 1;
-    //   constraints.weightx = 1;
-    //   constraints.weighty = 1;
-    //   constraints.fill = GridBagConstraints.BOTH;
-    //   panel2.add(label6, constraints);
+    JButton up = new JButton("^");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 6;
+    constraints.gridy = 1;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(up, constraints);
 
-    //   JLabel label9 = new JLabel("Label 9");
-    //   constraints.gridx = 2;
-    //   constraints.gridy = 0;
-    //   constraints.gridwidth = 1;
-    //   constraints.gridheight = 1;
-    //   constraints.weightx = 1;
-    //   constraints.weighty = 1;
-    //   constraints.fill = GridBagConstraints.BOTH;
-    //   panel3.add(label9, constraints);
+    JLabel label3 = new JLabel("");
+    label3.setBackground(Color.gray);
+    label3.setOpaque(true);
+    constraints = new GridBagConstraints();
+    constraints.gridx = 7;
+    constraints.gridy = 1;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(label3, constraints);
 
-    //   JButton up = new JButton("^");
-    //   constraints.gridx = 1;
-    //   constraints.gridy = 1;
-    //   constraints.gridwidth = 1;
-    //   constraints.gridheight = 1;
-    //   constraints.weightx = 1;
-    //   constraints.weighty = 1;
-    //   constraints.fill = GridBagConstraints.BOTH;
-    //   panel2.add(up, constraints);
+    JLabel items = new JLabel("Items", SwingConstants.CENTER);
+    constraints = new GridBagConstraints();
+    constraints.gridx = 0;
+    constraints.gridy = 2;
+    constraints.gridwidth = 5;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(items, constraints);
 
-    //   JButton left = new JButton("<");
-    //   constraints.gridx = 0;
-    //   constraints.gridy = 0;
-    //   constraints.gridwidth = 1;
-    //   constraints.gridheight = 1;
-    //   constraints.weightx = 1;
-    //   constraints.weighty = 1;
-    //   constraints.fill = GridBagConstraints.BOTH;
-    //   panel2.add(left, constraints);
+    JButton left = new JButton("<");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 5;
+    constraints.gridy = 2;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(left, constraints);
 
-    
+    JButton down = new JButton("V");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 6;
+    constraints.gridy = 2;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(down, constraints);
+
+    JButton right = new JButton(">");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 7;
+    constraints.gridy = 2;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(right, constraints);
+
+    //spacers
+    JLabel space0 = new JLabel("0");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 0; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space0, constraints);
+
+    JLabel space1 = new JLabel("1");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 1; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space1, constraints);
+
+    JLabel space2 = new JLabel("2");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 2; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space2, constraints);
+
+    JLabel space3 = new JLabel("3");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 3; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space3, constraints);
+
+    JLabel space4 = new JLabel("4");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 4; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space4, constraints);
+
+    JLabel space5 = new JLabel("5");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 5; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space5, constraints);
+
+    JLabel space6 = new JLabel("6");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 6; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space6, constraints);
+
+    JLabel space7 = new JLabel("7");
+    constraints = new GridBagConstraints();
+    constraints.gridx = 7; 
+    constraints.gridy = 3;
+    constraints.gridwidth = 1;
+    //constraints.gridheight = 1;
+    constraints.weightx = 1;
+    //constraints.weighty = 1;
+    //constraints.fill = GridBagConstraints.BOTH;
+    panelMain.add(space7, constraints);
+
+
     // // Create labels with fixed size
     // JLabel label1 = new JLabel("Label 1");
     // label1.setPreferredSize(new Dimension(200, 400));
