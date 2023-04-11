116ae7925db95d8bb2fa57235a63122129b22f12
John Patrick <yobyeh@gmail.com>
2023-04-10 22:49:27 -0400

    comments

--- src/Game/Palyer.java
+++ src/Game/Palyer.java
@@ -1,9 +1,27 @@
 package Game;
 
-// stores items in a hash table 
+import Structure.HashTable;
+
+//todo: display text dosent repeat "YAY YOU PICKED THIS UP" every time.
+//stores items in a hash table 
 //stores player position in x,y format 
 
 
 public class Palyer {
+
+//stores a hash table called inventory
+HashTable inventory = new HashTable();
+
+
+//remembers picked up items 
+public void giveItem(String s){
+    //inventory.addItem("Item Name");
+}
+
+
+
+
+
+
     
 }
--- src/Structure/HashTable.java
+++ src/Structure/HashTable.java
@@ -3,9 +3,34 @@ package Structure;
 //to store items the player has picked up
 //bool check given item String to see if it has been stored 
 //implemented in player class
-// sorted array output for view to display items in order 
+//sorted array output for view to display items in order 
 
 
 public class HashTable {
+
+    //have to pick table size
+    //is array of some type of list
+
+
+
+    //adds string to table
+    public void addItem(String s){
+
+
+
+    }
+
+    //returns true false if string is in table
+    public Boolean search(String s){
+
+        return null;
+    }
+
+    //hashing function to get key 
+
+
+
+
+    
     
 }
