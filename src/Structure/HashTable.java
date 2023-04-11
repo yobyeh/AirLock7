45bd37811d69529b0f3bfd58149d1969633a3dc2
KharloPena <113564953+KharloPena@users.noreply.github.com>
2023-04-11 16:35:01 -0700

    Implemented HashTable
    Implemented user inventory functions to HashTable.java

--- src/Structure/HashTable.java
+++ src/Structure/HashTable.java
@@ -1,36 +1,48 @@
 package Structure;
 
+//Importing packages
+import java.util.*;
+import java.util.set;
+
 //to store items the player has picked up
 //bool check given item String to see if it has been stored 
 //implemented in player class
 //sorted array output for view to display items in order 
 
-
 public class HashTable {
 
-    //have to pick table size
-    //is array of some type of list
+    //Instantiating user inventory HashTable
+    HashTable<Integer, String> userInventory = new HashTable<>();
 
+    //Function to add item to user inventory
+    public void addItem(Integer location, String item){
 
-
-    //adds string to table
-    public void addItem(String s){
-
-
-
+        //Accepts an Integer as location in the bag for the key
+        userInventory.put(location, item);
     }
 
-    //returns true false if string is in table
-    public Boolean search(String s){
+    //Function to search user inventory for items
+    public Boolean searchInventory(String item){
 
-        return null;
+        //Returning boolean for whether item exists in inventory or not
+        return userInventory.containsValue(item);
     }
 
-    //hashing function to get key 
+    //Function to get location/key for each item
+    public Integer getKey(String item){
 
+        //Getting number of keys from user inventory HashTable
+        Set<Integer> location = userInventory.keySet();
 
+        //Iterating over user intenvory
+        for(Integer key: location){
 
+            //If item is in user invetory return its key
+            if(userInventory.containsValue(item)){
 
-    
-    
+                return userInventory.getKey(item);
+            }
+
+        }
+    }
 }
