1b8913e434af80b9f3d56463cc9cf1e0c3b86ee1
KharloPena <113564953+KharloPena@users.noreply.github.com>
2023-04-23 18:46:51 -0700

    Clean up
    Clean of contribution section.

--- src/Game/App.java
+++ src/Game/App.java
@@ -1,21 +1,22 @@
 /**
 *This is the main code section for our project, AirLock7.
+*This project uses a modified hash table to store the items 
+*the player can pick up. If the game were to continue in 
+*development this would work well for storing a large number 
+*of items. This project also uses an array of stacks to hold 
+*all the text for the game when an event happens the stacks 
+*are popped.
 * 
 * @authors  John Patrick, Luis K. Pena, Sergio Costa
 * @version  1.0
 * @since    04/20/2023
 */
 
-// this project uses a modified hash table to store the items the player can pick up.
-// if the game were to continue in development this would work well for storing a large number of items also
+////-----------Contributions-----------////
 
-// this project aslo uses a an array of stacks to hold all the text for the game
-// when an event happens the stacks are popped
-
-//CONTRIBUTIONS
 // John Patrick: game logic, UI, testing, bugfixes.
-// Luis K. Pena: hash table, text, testing.
-// Sergio Costa:game map, map design, testing.
+// Luis K. Pena: hash table, story text events, testing.
+// Sergio Costa: game map, map design, testing.
 
 //Package(s)
 package Game;
